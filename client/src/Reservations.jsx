da90c3e900a05801f880b129600eb4a50b6de5e0
Derek Velzy <dmvelzy@gmail.com>
2020-11-29 09:42:30 -0800

    Use webp and jpeg2000 for file size optimizations

--- client/src/Reservations.jsx
+++ client/src/Reservations.jsx
@@ -5,12 +5,29 @@ import styles from '../../styles.css';
 
 const Reservations = () => (
   <div className={styles.container}>
-    <img className={styles.header} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_header.png" alt="header" />
+    {/* <img className={styles.header} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_header.png" alt="header" /> */}
+    <picture>
+      <source className={styles.header} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_header.webp" type="image/webp" />
+      <source className={styles.header} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_header.jp2" type="image/jp2" />
+      <img className={styles.header} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_header.png" alt="header" />
+    </picture>
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <div style={{ width: '52%' }}>
-        <img className={styles.airbnbsummary} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_summary.png" alt="summary" />
-        <img className={styles.description} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_description.png" alt="description" />
-        <img className={styles.description} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_amenities.png" alt="amenities" />
+        <picture>
+          <source className={styles.airbnbsummary} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_summary.webp" type="image/webp" />
+          <source className={styles.airbnbsummary} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_summary.jp2" type="image/jp2" />
+          <img className={styles.airbnbsummary} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_summary.png" alt="header" />
+        </picture>
+        <picture>
+          <source className={styles.description} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_description.webp" type="image/webp" />
+          <source className={styles.description} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_description.jp2" type="image/jp2" />
+          <img className={styles.description} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_description.png" alt="header" />
+        </picture>
+        <picture>
+          <source className={styles.description} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_amenities.webp" type="image/webp" />
+          <source className={styles.description} srcSet="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_amenities.jp2" type="image/jp2" />
+          <img className={styles.description} src="https://airbnb-fec-reservations.s3-us-west-1.amazonaws.com/airbnb_amenities.png" alt="header" />
+        </picture>
       </div>
       <Box />
     </div>
